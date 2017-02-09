a983bcb31af0a84c042e4e4413a9f3b262e05495
Erin Stanfill <erin.stanfill@versionone.com>
2017-02-09 16:37:11 -0500

    token magic (i hope)

--- app/slack2v1.js
+++ app/slack2v1.js
@@ -3,11 +3,10 @@ import {getMemberByEmail} from './v1-api';
 import {SLACK_API_TOKEN} from './env';
 
 export function getV1UserFromSlack(user_id) {
-	const url = "https://slack.com/api/users.info";
-	console.log("looking up user on slack", user_id);
-	return axios.get(url, {params:{token:SLACK_API_TOKEN,user:user_id}}).then(slack=> {
-		console.log(slack.data);
-		const email = slack.data.user.profile.email;
+	const url = `https://slack.com/api/users.profile.get?token=${SLACK_API_TOKEN}&user=${user_id}`;
+	console.log("looking up user on slack", user_id, url);
+	return axios.get(url).then(slack=> {
+		const email = slack.data.profile.email;
 		return getMemberByEmail(email).then(v1=>{
 			return v1.data[0][0]._oid;
 		})
