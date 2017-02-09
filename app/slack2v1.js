8037a9d8ec89cb2c1ef7bce94b2cd07e01c92730
Erin Stanfill <erin.stanfill@versionone.com>
2017-02-09 16:29:32 -0500

    error handling

--- app/slack2v1.js
+++ app/slack2v1.js
@@ -3,13 +3,14 @@ import {getMemberByEmail} from './v1-api';
 import {SLACK_API_TOKEN} from './env';
 
 export function getV1UserFromSlack(user_id) {
-	const url = `https://slack.com/api/users.info?token=${SLACK_API_TOKEN}&user=${user_id}`;
+	const url = "https://slack.com/api/users.info";
 	console.log("looking up user on slack", user_id);
-	return axios.get(url).then(slack=> {
+	return axios.get(url, {params:{token:SLACK_API_TOKEN,user:user_id}}).then(slack=> {
 		console.log(slack.data);
 		const email = slack.data.user.profile.email;
 		return getMemberByEmail(email).then(v1=>{
 			return v1.data[0][0]._oid;
-		});
-	});
+		})
+		.catch(v1Error => console.error("v1", slackError));
+	}).catch(slackError => console.error("slack", slackError));
 }
